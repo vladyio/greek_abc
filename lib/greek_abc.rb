339ebb8bd8d24c91ae695db8b815b3f40cbd6d80
Vladislav Andreev <me@vld.by>
2019-04-27 20:20:02 +0300

    Add LOWERS, UPPERS and their union

--- lib/greek_abc.rb
+++ lib/greek_abc.rb
@@ -1,4 +1,6 @@
 require 'greek_abc/version'
+
+require 'greek_abc/constants'
 require 'greek_abc/letter'
 require 'greek_abc/alphabet'
 
@@ -6,31 +8,4 @@ module GreekABC
   class GreekABCError < StandardError; end
   class LetterLookupError < GreekABCError; end
   class LetterNotFoundError < GreekABCError; end
-
-  ALPHABET = {
-    'Alpha' => 'α',
-    'Beta' => 'β',
-    'Gamma' => 'γ',
-    'Delta' => 'δ',
-    'Epsilon' => 'ε',
-    'Zeta' => 'ζ',
-    'Eta' => 'η',
-    'Theta' => 'θ',
-    'Iota' => 'ι',
-    'Kappa' => 'κ',
-    'Lambda' => 'λ',
-    'Mu' => 'μ',
-    'Nu' => 'ν',
-    'Xi' => 'ξ',
-    'Omicron' => 'ο',
-    'Pi' => 'π',
-    'Rho' => 'ρ',
-    'Sigma' => 'σ',
-    'Tau' => 'τ',
-    'Upsilon' => 'υ',
-    'Phi' => 'φ',
-    'Chi' => 'χ',
-    'Psi' => 'ψ',
-    'Omega' => 'ω'
-  }.freeze
 end
---
+++ lib/greek_abc/constants.rb
@@ -0,0 +1,33 @@
+module GreekABC
+
+  ALPHABET = {
+    'Alpha' => 'α',
+    'Beta' => 'β',
+    'Gamma' => 'γ',
+    'Delta' => 'δ',
+    'Epsilon' => 'ε',
+    'Zeta' => 'ζ',
+    'Eta' => 'η',
+    'Theta' => 'θ',
+    'Iota' => 'ι',
+    'Kappa' => 'κ',
+    'Lambda' => 'λ',
+    'Mu' => 'μ',
+    'Nu' => 'ν',
+    'Xi' => 'ξ',
+    'Omicron' => 'ο',
+    'Pi' => 'π',
+    'Rho' => 'ρ',
+    'Sigma' => 'σ',
+    'Tau' => 'τ',
+    'Upsilon' => 'υ',
+    'Phi' => 'φ',
+    'Chi' => 'χ',
+    'Psi' => 'ψ',
+    'Omega' => 'ω'
+  }.freeze
+
+  LOWERS = ALPHABET.values.freeze
+  UPPERS = LOWERS.map(&:upcase).freeze
+  LOWERS_AND_UPPERS = (LOWERS + UPPERS).freeze
+end
