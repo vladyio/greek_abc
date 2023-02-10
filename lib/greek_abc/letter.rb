16a83de2b875f224821345f0fd5cbeffb7e3775d
Vladislav Andreev <andreev@vlad.social>
2023-02-10 18:11:38 +0300

    Refactor Letter class
    Add `lower_alt` method and update text representation for it

--- lib/greek_abc/letter.rb
+++ lib/greek_abc/letter.rb
@@ -1,16 +1,20 @@
 module GreekABC
   class Letter
-    attr_reader :position, :name, :lower, :upper
+    attr_reader :position, :name, :lower, :lower_alt, :upper
 
-    def initialize(position:, name:, lower:, upper:)
+    def initialize(position:, name:, lower:, upper:, lower_alt: nil)
       @position = position
       @name = name
       @lower = lower
+      @lower_alt = lower_alt
       @upper = upper
     end
 
     def to_s
-      "Letter ##{@position} '#{@name}', lowercase '#{@lower}', uppercase '#{@upper}'"
+      lower_alt = " / #{@lower_alt}" if @lower_alt
+
+      "Letter ##{@position} '#{@name}', lowercase '#{@lower}" \
+      "#{lower_alt}', uppercase '#{@upper}'"
     end
   end
 end
