3a7651edee8aeac6aa96750f3c11746afa3e01ea
Vladislav Andreev <andreev@vlad.social>
2023-02-10 21:23:18 +0300

    Refactor exception classes

--- lib/greek_abc.rb
+++ lib/greek_abc.rb
@@ -1,11 +1,23 @@
-require 'greek_abc/version'
+# frozen_string_literal: true
 
+require 'greek_abc/version'
 require 'greek_abc/constants'
 require 'greek_abc/letter'
+require 'greek_abc/letter_lookup'
 require 'greek_abc/alphabet'
 
 module GreekABC
   class GreekABCError < StandardError; end
-  class LetterLookupError < GreekABCError; end
-  class LetterNotFoundError < GreekABCError; end
+
+  class LetterLookupParamsError < GreekABCError
+    def initialize(wrong_parameter)
+      super("`#{wrong_parameter}` is not an existing letter attribute")
+    end
+  end
+
+  class LetterNotFoundError < GreekABCError
+    def initialize(parameter, value)
+      super("Letter with `#{parameter}: #{value}` was not found")
+    end
+  end
 end
