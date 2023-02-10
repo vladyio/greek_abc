a2021d80ad4a6d99de07b35636bec73cb49e6f4e
Vladislav Andreev <andreev@vlad.social>
2023-02-10 18:37:34 +0300

    Add yard documentation

--- lib/greek_abc/letter.rb
+++ lib/greek_abc/letter.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 module GreekABC
+  #
+  # Represents a letter object
+  #
   class Letter
     attr_reader :position, :name, :lower, :lower_alt, :upper
 
+    # Initialize a new letter
+    #
+    # @param [Integer] position Letter position in the alphabet
+    # @param [String] name Letter name
+    # @param [String] lower Lowercase representation
+    # @param [String] upper Uppercase representation
+    # @param [String, nil] lower_alt Lowercase alternative represenation, if exists
+    # @return [Letter]
+    #
     def initialize(position:, name:, lower:, upper:, lower_alt: nil)
       @position = position
       @name = name
@@ -10,6 +24,9 @@ module GreekABC
       @upper = upper
     end
 
+    #
+    # String representation of a [Letter] object
+    #
     def to_s
       lower_alt = " / #{@lower_alt}" if @lower_alt
 
