24e0b92479d15c3c4c90e4e5adb68d40955b76e6
Vladislav Andreev <andreev@vlad.social>
2023-02-10 19:19:51 +0300

    Add yard documentation for attributes

--- lib/greek_abc/letter.rb
+++ lib/greek_abc/letter.rb
@@ -4,16 +4,26 @@ module GreekABC
   #
   # Represents a letter object
   #
+  # @!attribute [r] position
+  #   @return [Integer] letter position in the alphabet
+  # @!attribute [r] name
+  #   @return [String] letter name
+  # @!attribute [r] lower
+  #   @return [String] lowercase representation
+  # @!attribute [r] lower_alt
+  #   @return [String, nil] lowercase alternative represenation, if exists
+  # @!attribute [r] upper
+  #   @return [String] uppercase representation
   class Letter
     attr_reader :position, :name, :lower, :lower_alt, :upper
 
     # Initialize a new letter
     #
-    # @param [Integer] position Letter position in the alphabet
-    # @param [String] name Letter name
-    # @param [String] lower Lowercase representation
-    # @param [String] upper Uppercase representation
-    # @param [String, nil] lower_alt Lowercase alternative represenation, if exists
+    # @param [Integer] position (see {position})
+    # @param [String] name (see {name})
+    # @param [String] lower (see {lower})
+    # @param [String] upper (see {upper})
+    # @param [String, nil] lower_alt (see {lower_alt})
     # @return [Letter]
     #
     def initialize(position:, name:, lower:, upper:, lower_alt: nil)
@@ -25,8 +35,8 @@ module GreekABC
     end
 
     #
-    # String representation of a [Letter] object
-    #
+    # String representation of a {Letter} object
+    # @return [String]
     def to_s
       lower_alt = " / #{@lower_alt}" if @lower_alt
 
