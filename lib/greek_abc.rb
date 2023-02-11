39fe12adef173e9b9d52629f59b3a3a497899fd7
Vladislav Andreev <andreev@vlad.social>
2023-02-11 11:16:24 +0300

    Add documentation for error classes

--- lib/greek_abc.rb
+++ lib/greek_abc.rb
@@ -9,12 +9,14 @@ require 'greek_abc/alphabet'
 module GreekABC
   class GreekABCError < StandardError; end
 
+  # Raised when the lookup parameter is not a valid {Letter} attribute
   class LetterLookupParamsError < GreekABCError
     def initialize(wrong_parameter)
       super("`#{wrong_parameter}` is not an existing letter attribute")
     end
   end
 
+  # Raised when the passed parameter and value didn't match any {Letter}
   class LetterNotFoundError < GreekABCError
     def initialize(parameter, value)
       super("Letter with `#{parameter}: #{value}` was not found")
