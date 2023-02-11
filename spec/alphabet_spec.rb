b64f3ca8f8699aa495a869923b544c0e399d5030
Vladislav Andreev <andreev@vlad.social>
2023-02-11 12:06:48 +0300

    Add spec for Alphabet and LetterLookup

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -1,2 +1,33 @@
 RSpec.describe GreekABC::Alphabet do
+  subject(:alphabet) { GreekABC::Alphabet.new }
+  let(:alphabet_correct_size) { 24 }
+
+  describe '#letters' do
+    it 'returns a full array of all letters (Array<Letter>)' do
+      letters = alphabet.letters
+
+      expect(letters.size).to eq(alphabet_correct_size)
+    end
+  end
+
+  describe '#to_h' do
+    it 'returns a hash represention of alphabet with all letter attributes' do
+      hash = alphabet.to_h
+
+      expect(hash).to include(
+        'Alpha' => { position: 1, lower: 'α', lower_alt: nil, upper: 'Α' },
+        'Sigma' => { position: 18, lower: 'σ', lower_alt: 'ς', upper: 'Σ' },
+        'Omega' => { position: 24, lower: 'ω', lower_alt: nil, upper: 'Ω' }
+      )
+    end
+  end
+
+  describe '#find_letter' do
+    it 'returns a Letter instance fitting lookup params' do
+      letter = alphabet.find_letter(name: 'Sigma')
+
+      expect(letter).to be_a(GreekABC::Letter)
+      expect(letter.name).to eq('Sigma')
+    end
+  end
 end
--- spec/letter_lookup_spec.rb
+++ spec/letter_lookup_spec.rb
@@ -1,2 +1,29 @@
 RSpec.describe GreekABC::LetterLookup do
+  let(:letters) { GreekABC::Alphabet.new.letters }
+  subject(:lookup) { GreekABC::LetterLookup.new(letters) }
+
+  let(:correct_params) { Hash[:name, 'Omega'] }
+  let(:incorrect_attr) { :location }
+  let(:incorrect_attr_value) { 100 }
+
+  describe '#find_letter' do
+    it 'returns a matching letter for correct & existing param/value' do
+      result = lookup.find_letter(**correct_params)
+
+      expect(result).to be_a(GreekABC::Letter)
+      expect(result.position).to eq(letters.size)
+    end
+
+    it 'raises a LetterLookupParamsError with an unexisting letter attribute' do
+      expect do
+        lookup.find_letter(**Hash[incorrect_attr, incorrect_attr_value])
+      end.to raise_error(GreekABC::LetterLookupParamsError)
+    end
+
+    it 'raises a LetterNotFoundError with for an unexisting value' do
+      expect do
+        lookup.find_letter(**Hash[:position, incorrect_attr_value])
+      end.to raise_error(GreekABC::LetterNotFoundError)
+    end
+  end
 end
